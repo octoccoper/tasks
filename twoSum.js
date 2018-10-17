8b04b1ed3e14f404a36beef1284d3a2229cbc0cc
octoccoper <octocopper@gmail.com>
2018-10-17 19:20:00 +0300

    added first task

---
+++ twoSum.js
@@ -0,0 +1,15 @@
+/* @param {number[]} nums
+@param {number} target @return {number[]}
+*/
+var twoSum = function(nums, target) {
+    var arr = [];
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(target === nums[i] + nums[j]) {
+                arr.push(i);
+                arr.push(j);
+            }
+        }
+    }
+    return arr;
+};
