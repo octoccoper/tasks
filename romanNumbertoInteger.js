40c987827d3dc0ad459adfe8ebddd4658b5a5996
octoccoper <octocopper@gmail.com>
2018-11-01 15:12:54 +0200

    rewritten logic in romanNumbertoInteger.js

--- romanNumbertoInteger.js
+++ romanNumbertoInteger.js
@@ -1,57 +1,63 @@
-var output = 0;
-var result = 0;
-var romanToInt = function(s) {
-var romanNumber = s.split("");
-if(romanNumber.length > 0) {
-for(var i = 0; i < romanNumber.length; i++) {
-switch(romanNumber[i]) {
-    case "I":
-        romanNumber[i] = 1;
-result = result + romanNumber[i];
-		break;
-    case "V":
-        romanNumber[i]= 5;
-result = result + romanNumber[i];
-		break;
-    case "X":
-        romanNumber[i] = 10;
-result = result + romanNumber[i];
-		break;
-    case "L":
-        romanNumber[i] = 50;
-result = result + romanNumber[i];
-		break;
-    case "C":
-        romanNumber[i] = 100;
-result = result + romanNumber[i];
-		break;
-    case "D":
-        romanNumber[i] = 500;
-result = result + romanNumber[i];
-		break;
-    case "M":
-        romanNumber[i] = 1000;
-result = result + romanNumber[i];
-		break;
-    default:
-	romanNumber = undefined;
-       }
 
-for (var j = 1; j < romanNumber.length; j++) {
+var romanToInt = function (s) {
+    var output = 0;
+    var result = 0;
+    if (s.length > 0) {
+        for (var i = 0; i < s.length; i++) {
+            switch (s[i]) {
+                case "I":
+                    result = result + 1;
+                    break;
+                case "V":
+                    result = result + 5;
+                    break;
+                case "X":
+                    result = result + 10;
+                    break;
+                case "L":
+                    result = result + 50;
+                    break;
+                case "C":
+                    result = result + 100;
+                    break;
+                case "D":
+                    result = result + 500;
+                    break;
+                case "M":
+                    result = result + 1000;
+                    break;
+                default:
+                    result = 0;
+            }
+        }
 
-if(romanNumber[j] < romanNumber[i]) {
-output = (romanNumber[i] + romanNumber[j]) - (romanNumber[i] - romanNumber[j]);
-result = result - output;
-}
-}
-}
-}
- else if (romanNumber.length < 0){
-	result = -1;
-}
-if((s.length == 2) && (romanNumber[1] > romanNumber[0])) {
-result = romanNumber[1] - romanNumber[0];
-}
+        if (s.indexOf("IV") !== -1) {
+            result = result - 2;
+        }
 
-return result;
-};
+        if (s.indexOf("IX") !== -1) {
+            result = result - 2;
+        }
+
+        if (s.indexOf("XL") !== -1) {
+            result = result - 20;
+        }
+
+        if (s.indexOf("XC") !== -1) {
+            result = result - 20;
+        }
+
+        if (s.indexOf("CD") !== -1) {
+            result = result - 200;
+        }
+
+        if (s.indexOf("CM") !== -1) {
+            result = result - 200;
+        }
+
+        if (s.indexOf("DM") !== -1) {
+            result = result - 1000;
+        }
+        return console.log(result);
+    }
+}
