4799d8523c0729324679a4d0ecee52d1bc4b8527
octoccoper <octocopper@gmail.com>
2018-11-01 13:07:53 +0200

    changed romanNumbertoInteger.js

--- romanNumbertoInteger.js
+++ romanNumbertoInteger.js
@@ -1,4 +1,5 @@
 var output = 0;
+var result = 0;
 var romanToInt = function(s) {
 var romanNumber = s.split("");
 if(romanNumber.length > 0) {
@@ -7,43 +8,54 @@ switch(romanNumber[i]) {
     case "I":
         romanNumber[i] = 1;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "V":
         romanNumber[i]= 5;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "X":
         romanNumber[i] = 10;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "L":
         romanNumber[i] = 50;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "C":
         romanNumber[i] = 100;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "D":
         romanNumber[i] = 500;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     case "M":
         romanNumber[i] = 1000;
 console.log(romanNumber[i]);
+result = result + romanNumber[i];
 		break;
     default:
 	romanNumber = undefined;
        }
+
 for (var j = 1; j < romanNumber.length; j++) {
 console.log("yes, we are in second for!");
 if(romanNumber[j] < romanNumber[i]) {
 console.log("romanNumber[i]",romanNumber[i]);
 console.log("romanNumber[j]",romanNumber[j]);
-output = output + romanNumber[i] - romanNumber[j]; 
-console.log(output);
+output = (romanNumber[i] + romanNumber[j]) - (romanNumber[i] - romanNumber[j]);
+result = result -  (romanNumber[i] - romanNumber[j]) + output;
+console.log("output = ",output);
+console.log("result = result -  (romanNumber[i] - romanNumber[j]) + output = ",result);
 }
 }
+
 }
  }
  else {
@@ -52,4 +64,5 @@ console.log(output);
 
 console.log(romanNumber);
 console.log(output);
+console.log(result);
 };
