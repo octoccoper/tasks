6a29c253130c1556158ec626c415eb32aa4eae31
octoccoper <octocopper@gmail.com>
2018-11-06 17:02:52 +0200

    updated longestCommonPrefix.js

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,14 +1,19 @@
 var longestCommonPrefix = function(strs) {
-var resultStr="";
+var resultStr="", resultStrTmp="";
 var sortStr = strs.sort(function(a, b){return a.length - b.length});
-for(var i = 0; i < sortStr.length; i++) {
+var strFirst = sortStr[0];
+for(var i = 0; i < strFirst.length; i++) {
 var tempStr = sortStr[i];
 for(var j = 0; j < tempStr.length; j++){
+if(sortStr[i + 1]!== undefined){
 		if(sortStr[i][j] === sortStr[i + 1][j]) {
-			resultStr = resultStr.concat(sortStr[i][j]);
-			console.log(resultStr);
-        }
+			resultStrTmp = resultStrTmp.concat(sortStr[i][j]);
+			console.log(resultStrTmp);
+                }
+
+}
 	}
+
   }
 return resultStr;	
 };
