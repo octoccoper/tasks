32bea98c2db126f53133fc44bf43a28833f6cbb2
octoccoper <octocopper@gmail.com>
2018-11-08 19:07:12 +0200

    changed longestCommonPrefix.js

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,52 +1,13 @@
-var longestCommonPrefix = function(strs) {
-var resultStr="";
-var tempStr=[];
-if(strs.length === 1) {
-	resultStr = strs[0];
-	return resultStr;
-} 
-else {
-var sortStr = strs.sort(function(a, b){return a.length - b.length});
-var strFirst = sortStr[0];
-for(var i = 0; i < sortStr.length; i++) {
-resultStr = "";
-
-for(var j = 0; j < strFirst.length; j++){
-if(sortStr[i + 1]!== undefined && sortStr[i]!== undefined){
-		if(sortStr[i][j] === sortStr[i + 1][j]) {
-			resultStr = resultStr + sortStr[i][j];
-			console.log("sortStr[i][j] = ",sortStr[i][j],"resultStr=", resultStr);
+var longestCommonPrefix = function(arr) { 
+    var prefix = (arr.length) ? arr[0] : '';
+    var i = 1;
+    
+    while (prefix && i < arr.length) {
+        var next = arr[i];   
+        while (next.indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
         }
-		else {
-			resultStr = "";
-			break;
-		}
-	}
-
-	}
-if(resultStr.length > 0) {
-tempStr.push(resultStr);
-console.log("tempStr",tempStr);
+        i++;
+    }
+    return prefix;
 }
-}
-resultStr = "";
-for(var k = 0; k < tempStr.length; k++){
-if(tempStr[k + 1]!== undefined) {
-	if(tempStr[k] < tempStr[k + 1]){
-		resultStr = tempStr[k];
-		console.log("resultStr",resultStr);
-	}
-	else {
-		resultStr = tempStr[k + 1];
-		console.log("resultStr",resultStr);
-	}
-}
-	if(tempStr[k] === tempStr[k + 1] || tempStr.length === 1) {
-	resultStr = tempStr[k];
-		console.log("resultStr",resultStr);
-	}
-}
-
-return resultStr; 
-}	
-};
