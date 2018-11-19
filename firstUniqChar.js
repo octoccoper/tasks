56a2b0f86d3f7803316bd152c9895c8620346cc1
octoccoper <octocopper@gmail.com>
2018-11-19 18:21:24 +0200

    updated task firstUniqChar.js

--- firstUniqChar.js
+++ firstUniqChar.js
@@ -1,18 +1,19 @@
 var firstUniqChar = function(s) {
-var temp;
-
-if(s.length === 0) {
-	return 0;	
+var indexData;
+if(s.length === 0 || (typeof s !== "string")) {
+	indexData = -1;
 }
-
-if(s.length > 0) {
-    for(var i = 0; i < s.length; i++) {
-		if((s[i]!==s[i+1]) && (s[i+1]!==s[i+2])) {
-			return	temp = i+1;
-		}
-		else temp =-1;
-	}
-} 
-
-return temp;
+else {
+    for(var i = 0; i < s.length; i++){
+		console.log("current i = ", i );
+		console.log("s.charAt(i) = ",s.charAt(i) );
+		console.log("s.lastIndexOf(s.charAt(i))",s.lastIndexOf(s.charAt(i)));
+        if(s.charAt(i) === s.lastIndexOf(s.charAt(i)){
+			indexData = i;
+			break;
+        }
+        
+    }
+}
+return indexData;
 };
