7945dcede7d9a79ad77d3ef4d36fa565617e0ad4
octoccoper <octocopper@gmail.com>
2018-11-20 18:15:13 +0200

    updated structure of firstUniqChar.js

--- firstUniqChar.js
+++ firstUniqChar.js
@@ -1,14 +1,14 @@
-var firstUniqChar = function(s) {
- if (s.length === 0 || (typeof s !== "string")) {
+var firstUniqChar = function (s) {
+    if (s.length === 0 || (typeof s !== "string")) {
         return -1;
     }
-else {
-  for (var i = 0; i < s.length; i++) {
-        var c = s.charAt(i);
-        if (s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
-            return i;
+    else {
+        for (var i = 0; i < s.length; i++) {
+            var c = s.charAt(i);
+            if (s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
+                return i;
+            }
         }
+        return -1;
     }
-    return -1;
-}
-}
+};
