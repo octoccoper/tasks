ce775a3161b3238378f9dc6e07d439de84dd2fa9
Kate <43202688+octoccoper@users.noreply.github.com>
2019-02-18 20:10:43 +0200

    Update uniqueMorseRepresentations.js

--- uniqueMorseRepresentations.js
+++ uniqueMorseRepresentations.js
@@ -2,8 +2,13 @@
  * @param {string[]} words
  * @return {number}
  */
-var uniqueMorseRepresentations = function(words) {
-    var arrLetters = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-
-
+var uniqueMorseRepresentations = function (words) {
+    var arrLetters = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var newArr = [];
+    for (var i = 0; i < words.length; i++) {
+        newArr[i] = words [i];
+        console.log(newArr[i]);
+    }
+    console.log(newArr);
 };
