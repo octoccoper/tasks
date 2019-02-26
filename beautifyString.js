f17a901e27bf15101777125f0c10d50b3428aae8
octoccoper <octocopper@gmail.com>
2019-02-26 20:00:42 +0200

    updated beautifyString.js

--- beautifyString.js
+++ beautifyString.js
@@ -1,5 +1,12 @@
 function beautifyString(org, excl) { 
   let pattern = new RegExp(/[^A-z]*/g);
-  let newString = org.replace(pattern,'');
+  let newString = org.replace(pattern, '');
+  for (let i = 0; i < temp.length; i++) {
+    if (org[i].match(/\\d+\\.?\\d*/g)) {
+      org.splice(i, 1);
+    } else { 
+      newString += org[i];
+    }
+  }
   return newString;
 }
