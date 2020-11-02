e856b9e863f7ac04141923bc8b746eab3b8f1754
octoccoper <octocopper@gmail.com>
2020-11-02 17:45:51 +0200

    updating function isStringPalindrome.js

--- isStringPalindrome.js
+++ isStringPalindrome.js
@@ -1,12 +1,17 @@
 function isStringPalindrome(str){
 
+let reverseStr;
 
- if(typeof str === "string") {
-	console.log("success");
-	return str;
-	} else {
-	console.log("Entered parameter of isStringPalindrome function isn't string, it's ", typeof str);	
-	return str;
-    }
+ if(typeof str !== "string") {
+    return false;
+ }
+
+reverseStr = str.split('').reverse().join('');
+
+if(str === reverseStr) {
+    return true;
+} else {
+    return false;
+}
+
 } 
-
